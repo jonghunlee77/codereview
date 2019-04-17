7b36053d7195f4b694dc304ac3cfe393d3d0ca3b
jonghunlee77 <49699846+jonghunlee77@users.noreply.github.com>
2019-04-17 15:55:37 +0900

    Update goods.cc

--- goods.cc
+++ goods.cc
@@ -1,5 +1,9 @@
 #include "./goods.h"
 
+Goods::Goods(int index, int price)
+    : goods_index_(index), goods_price(price) {
+}
+
 // Set member variable of goods with given index and price values
 void Goods::SetGoodsInfo(int index, int price) {
   goods_index_ = index;
